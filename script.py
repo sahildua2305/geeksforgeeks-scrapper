d0473bfa07d6ba660dd0ebdfe93d7fda117c416e
Sahil <sahildua2305@gmail.com>
2014-07-02 02:31:38 +0530

    Try to get code

--- script.py
+++ script.py
@@ -9,4 +9,6 @@ pageFile.close()
 soup = BeautifulSoup((pageHtml))
 
 for code in soup.find_all('pre'):
-    print(code)
+    print(code.get_text())
+    if code['class']:
+        print code['class']
